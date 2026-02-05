440796b01b1c631000f58d99294b6b205e04f005
swetha2412 <swethaalugubelly2412@gmail.com>
2026-02-04 20:48:04 -0500

    Create utils.py
    Utilities

---
+++ utils.py
@@ -0,0 +1,11 @@
+import random
+import string
+import pyperclip
+
+def generate_password(length=16):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*"
+    return "".join(random.choice(chars) for _ in range(length))
+
+def copy_clipboard(text):
+    pyperclip.copy(text)
+    print("Copied to clipboard.")

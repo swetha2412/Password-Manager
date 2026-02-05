d33c1b38d0559515372b128f1dec0855b39715ce
swetha2412 <swethaalugubelly2412@gmail.com>
2026-02-04 20:49:56 -0500

    Update main.py
    Main

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
-import random
-import string
-import pyperclip
+import os
+from rich import print
+from crypto import derive_key
+from auth import setup_master, login
+from storage import load_vault, save_vault
+from utils import generate_password, copy_clipboard
 
-def generate_password(length=16):
-    chars = string.ascii_letters + string.digits + "!@#$%^&*"
-    return "".join(random.choice(chars) for _ in range(length))
+if not os.path.exists("master.key"):
+    setup_master()
 
-def copy_clipboard(text):
-    pyperclip.copy(text)
-    print("Copied to clipboard.")
+if not login():
+    print("[red]Access Denied[/red]")
+    exit()
+
+master_pw = input("Re-enter master password: ")
+key = derive_key(master_pw)
+vault = load_vault(key)
+
+while True:
+    print("\n[bold cyan]Password Manager[/bold cyan]")
+    print("1. Add")
+    print("2. View")
+    print("3. Update")
+    print("4. Delete")
+    print("5. Generate Password")
+    print("6. Exit")
+
+    choice = input("> ")
+
+    if choice == "1":
+        site = input("Site: ")
+        user = input("Username: ")
+        pw = input("Password: ")
+        vault[site] = {"user": user, "pw": pw}
+        save_vault(vault, key)
+
+    elif choice == "2":
+        site = input("Site: ")
+        if site in vault:
+            print(vault[site])
+            copy_clipboard(vault[site]["pw"])
+
+    elif choice == "3":
+        site = input("Site: ")
+        if site in vault:
+            vault[site]["pw"] = input("New password: ")
+            save_vault(vault, key)
+
+    elif choice == "4":
+        site = input("Site: ")
+        vault.pop(site, None)
+        save_vault(vault, key)
+
+    elif choice == "5":
+        pw = generate_password()
+        print("Generated:", pw)
+        copy_clipboard(pw)
+
+    elif choice == "6":
+        break
